192768b4c6a5788a750421c1ade76c1c4f0b0b1e
Vanja Hug <113890523+vanvan7@users.noreply.github.com>
2022-12-13 20:36:29 +0100

    This file we have to create

---
+++ src/main/java/Client/PersistenceClient.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Client;
+
+import Exceptions.AlreadyExistsException;
+import Exceptions.DoesNotExistException;
+import Models.Restaurant;
+import Models.User;
+import java.util.List;
+import java.io.IOException;
+import java.io.StringReader;
+import java.util.ArrayList;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.WebTarget;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
+/**
+ *
+ * @author User
+ */
+public class PersistenceClient {
+    
+}
